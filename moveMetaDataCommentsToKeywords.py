8b3c98179b252b707a26a5a614a6242a08ef7ab1
creacominc <haroldt@creacom.ca>
2020-10-20 11:49:08 -0400

    changed logic to iterate over subfolders

--- moveMetaDataCommentsToKeywords.py
+++ moveMetaDataCommentsToKeywords.py
@@ -2,31 +2,39 @@
 from setupEnv import getResolveObj
 import sys
 
+def iterateFolder( folder ):
+    clips = folder.GetClipList()
+    for clip in clips :
+        properties = clip.GetClipProperty()
+        name      = properties['Clip Name']
+        optimized = properties['Optimized Media']
+        clipType  = properties['Type']
+        usage     = int(properties['Usage'])
+        comments = properties['Comments']
+        keywords    = properties['Keyword']
+
+        metaData = clip.GetMetadata()
+        if ( 'Comments' in metaData and (len(metaData['Comments'].strip()) > 0)):
+            comments = metaData[ 'Comments' ].strip()
+            if ( 'Keywords' in metaData  and (len(metaData['Keywords'].strip().strip(",")) > 0)):
+                keywords = metaData[ 'Keywords' ].strip()
+                print( 'Appending comments (' + comments + ') to keywords (' + keywords + ')' )
+                clip.SetMetadata( 'Keywords', keywords + ',' + comments )
+            else:
+                print( 'Copying comments (' + comments + ') to keywords' )
+                clip.SetMetadata( 'Keywords',  comments )
+                clip.SetMetadata( 'Comments', '' )
+
+    subFolders = folder.GetSubFolderList()
+    for sub in subFolders:
+        iterateFolder( sub )
+
+
+
 resolve = getResolveObj()
 projectManager = resolve.GetProjectManager()
 project = projectManager.GetCurrentProject()
 mediaPool = project.GetMediaPool()
 rootFolder = mediaPool.GetRootFolder()
-
-clips = rootFolder.GetClipList()
-for clip in clips :
-    properties = clip.GetClipProperty()
-    name      = properties['Clip Name']
-    optimized = properties['Optimized Media']
-    clipType  = properties['Type']
-    usage     = int(properties['Usage'])
-    description = properties['Description']
-    keywords    = properties['Keyword']
-
-    metaData = clip.GetMetadata()
-    if ( 'Description' in metaData ):
-        description = metaData[ 'Description' ].strip()
-        if ( 'Keywords' in metaData  and (len(metaData['Keywords'].strip().strip(",")) > 0)):
-            keywords = metaData[ 'Keywords' ].strip()
-            print( 'Appending description (' + description + ') to keywords (' + keywords + ')' )
-            clip.SetMetadata( 'Keywords', keywords + ',' + description )
-        else:
-            print( 'Copying description (' + description + ') to keywords' )
-            clip.SetMetadata( 'Keywords',  description )
-        clip.SetMetadata( 'Description', '' )
+iterateFolder( rootFolder )
 
--- removeAllKeywords.py
+++ removeAllKeywords.py
@@ -2,24 +2,31 @@
 from setupEnv import getResolveObj
 import sys
 
+def iterateFolder( folder ):
+    clips = folder.GetClipList()
+    for clip in clips :
+        properties = clip.GetClipProperty()
+        name      = properties['Clip Name']
+        optimized = properties['Optimized Media']
+        clipType  = properties['Type']
+        usage     = int(properties['Usage'])
+        description = properties['Description']
+        keywords    = properties['Keyword']
+        metaData = clip.GetMetadata()
+
+        if ( 'Keywords' in metaData ):
+            keywords = metaData['Keywords']
+            print( 'Setting Keywords (' + keywords + ') to an empty string for clip: ' + name )
+            clip.SetMetadata( 'Keywords', '' )
+
+    subFolders = folder.GetSubFolderList()
+    for sub in subFolders:
+        iterateFolder( sub )
+
+
 resolve = getResolveObj()
 projectManager = resolve.GetProjectManager()
 project = projectManager.GetCurrentProject()
 mediaPool = project.GetMediaPool()
 rootFolder = mediaPool.GetRootFolder()
-
-clips = rootFolder.GetClipList()
-for clip in clips :
-    properties = clip.GetClipProperty()
-    name      = properties['Clip Name']
-    optimized = properties['Optimized Media']
-    clipType  = properties['Type']
-    usage     = int(properties['Usage'])
-    description = properties['Description']
-    keywords    = properties['Keyword']
-    metaData = clip.GetMetadata()
-
-    if ( 'Keywords' in metaData ):
-        keywords = metaData['Keywords']
-        print( 'Setting Keywords (' + keywords + ') to an empty string for clip: ' + name )
-        clip.SetMetadata( 'Keywords', '' )
+iterateFolder( rootFolder )
