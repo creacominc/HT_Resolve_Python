16790a2bd81bb5a2fa889434cf36f155a1b08576
creacominc <haroldt@creacom.ca>
2020-10-20 11:06:10 -0400

    added listClipDescriptionAndKeywords.py

---
+++ listClipDescriptionAndKeywords.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setupEnv import getResolveObj
+import sys
+
+resolve = getResolveObj()
+projectManager = resolve.GetProjectManager()
+project = projectManager.GetCurrentProject()
+mediaPool = project.GetMediaPool()
+rootFolder = mediaPool.GetRootFolder()
+
+clips = rootFolder.GetClipList()
+for clip in clips :
+    properties = clip.GetClipProperty()
+    name      = properties['Clip Name']
+    optimized = properties['Optimized Media']
+    clipType  = properties['Type']
+    usage     = int(properties['Usage'])
+    description = properties['Description']
+    keywords    = properties['Keyword']
+    metaData = clip.GetMetadata()
+
+    print( 'Clip: [' + name + '],  Description: [' + description + '], Keywords: [' + keywords + '], Usage: [' + str(usage) + '], Optimized: [' + optimized + ']' )
+
